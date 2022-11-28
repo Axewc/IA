346c7c3581a5fb38deb3cb57c3c670a1ef17869f
Axewc <axew@ciencias.unam.mx>
2022-11-28 14:59:25 -0600

    Datos por sript y documentación actualizada

--- Tarea2/src/IA43.py
+++ Tarea2/src/IA43.py
@@ -1,42 +1,34 @@
 
 
-# Shannon Entropy Calculator
-# parameter: data - string to calculate entropy
-# parameter: iterator - number of characters to iterate over
-# parameter: base - base of the logarithm
+# Calculadora de entropía de Shannon
+# parametro data: cadena para calcular la entropía
+# parametro iterator: número de iteraciones para calcular la entropía
+# parametro base: base del logaritmo
 from collections import Counter
 import math
 
-
+# Función para calcular la entropía de Shannon
 def shannon_entropy(data, iterator = 1, base = 2):
     if iterator > len(data):
         return 0
-    freqdist = Counter(data[i:i+iterator] for i in range(len(data)-iterator+1))
-    probs = [freqdist[ch]/len(data) for ch in freqdist]
-    entropy = - sum([p * math.log(p, base) for p in probs])
+    freqdist = Counter(data[i:i+iterator] for i in range(len(data)-iterator+1)) # Contador de frecuencias
+    probs = [freqdist[ch]/len(data) for ch in freqdist] # Probabilidad de cada símbolo
+    entropy = - sum([p * math.log(p, base) for p in probs]) # Entropía
     return entropy
 
-# main function
+# Función main para probar el código
 def main():
-    # read file
-    with open("data.txt", "r") as f:
-        data = f.read()
     # calculate entropy
     data1 = '010100110001110101001100011101010011000111'
     data2 = '000000000000000000000000000000000000000000'
     data3 = '010101010101101101110110010101010010111010'
     
-    entropy = shannon_entropy(data, iterator=1)
-    entropy1 = shannon_entropy(data, iterator=1)
+    entropy1 = shannon_entropy(data1, iterator=1)
     entropy2 = shannon_entropy(data2, iterator=1)
     entropy3 = shannon_entropy(data3, iterator=1)
     # print result
-    print("Entropy: %s" % entropy)
     print("Entropy: %s" % entropy1)
     print("Entropy: %s" % entropy2)
     print("Entropy: %s" % entropy3)
     
 main();
-# 010100110001110101001100011101010011000111
-# 000000000000000000000000000000000000000000
-# 010101010101101101110110010101010010111010
