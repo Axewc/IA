a889097722082ee9b90c7b4823845a629105a1fe
IgnacioRG <41647228+IgnacioRG@users.noreply.github.com>
2022-12-14 14:55:31 -0600

    Añadida la opcion middle button y simbolos

--- Practica2/src/simulacion.py
+++ Practica2/src/simulacion.py
@@ -2,17 +2,78 @@ import tkinter as tk
 
 basura = "#"
 obstaculo = "@"
+libre = "-"
+botchita = "Q"
 
 coordx = 0
 coordy = 0
 numlo = 0
 Cuarto = {}
+Simbolos = {}
+pos = 0
+prevPosition = 0
 
 def left_click(eve):
-    eve.widget.config(text=basura)
+    Simbolos[int(eve.widget.cget('text').replace(" ","").replace("\n","").replace("-","").replace("@","").replace("#",""))] = basura
+    eve.widget.config(text=eve.widget.cget('text').replace("-",basura).replace("@",basura))
+
+def mid_click(eve):
+    Simbolos[int(eve.widget.cget('text').replace(" ","").replace("\n","").replace("#","").replace("@","").replace("-",""))] = libre
+    eve.widget.config(text=eve.widget.cget('text').replace("#",libre).replace("@",libre))
 
 def right_click(eve):
-    eve.widget.config(text=obstaculo)
+    Simbolos[int(eve.widget.cget('text').replace(" ","").replace("\n","").replace("-","").replace("#","").replace("@",""))] = obstaculo
+    eve.widget.config(text=eve.widget.cget('text').replace("-",obstaculo).replace("#",obstaculo))
+
+def derecha():
+    global pos
+    par = (pos - 9) / 10
+
+    if pos % 10 != 9 or pos == 0 or par % 2 == 0 or pos == 9 and pos <= 199:
+      
+        print("posición anterior: " + str(pos))
+        pos += 1
+        print("posición nueva: " + str(pos))
+        Cuarto[pos].config(text=botchita, fg='pink')
+        Cuarto[pos-1].config(text=pos-1, fg='black')
+    else:
+        print("no me puedo mover más a la derecha")
+
+def izquierda():
+    global pos
+    par = pos / 10
+    if par % 2 != 0:
+        print("posición anterior: " + str(pos))
+        pos -= 1
+        print("posición nueva: " + str(pos))
+        Cuarto[pos].config(text=botchita,fg='pink')
+        Cuarto[pos+1].config(text=pos+1, fg='black')
+    else:
+        print("no me puedo mover más a la izquierda")
+
+def abajo():
+    global pos
+
+    if(pos + 20 <= 199):
+        print("posición anterior: " + str(pos))
+        pos += 20
+        print("posición nueva: " + str(pos))
+        Cuarto[pos].config(text=botchita, fg='pink')
+        Cuarto[pos-20].config(text=pos-20, fg='black')
+    else:
+        print("no me puedo ir más abajo")
+
+def arriba():
+    global pos
+
+    if(pos - 20 >= 0):
+        print("posición anterior: " + str(pos))
+        pos -= 20
+        print("posición nueva: " + str(pos))
+        Cuarto[pos].config(text=botchita, fg='pink')
+        Cuarto[pos+20].config(text=pos+20, fg='black')
+    else:
+        print("no me puedo ir más arriba")
 
 window = tk.Tk()
 window.title("Botchita simulacion")
@@ -32,9 +93,21 @@ form.pack(side=tk.LEFT)
 #bo = tk.Button(master=form, text="Agregar un Obstaculo", relief=tk.RAISED)
 #bo.place(x=20,y=150)
 
-#Inicio de la Simulacion.
-bsim = tk.Button(master=form, text="Iniciar simulacion", relief=tk.RAISED)
-bsim.place(x=30,y=200)
+#Moverse derecha
+bsim = tk.Button(master=form, text="►", relief=tk.RAISED, command= derecha)
+bsim.place(x=110,y=100)
+
+#Moverse izquierda
+bsim = tk.Button(master=form, text="◄", relief=tk.RAISED, command= izquierda)
+bsim.place(x=50,y=100)
+
+#Moverse abajo
+bsim = tk.Button(master=form, text="▼", relief=tk.RAISED, command= abajo)
+bsim.place(x=80,y=130)
+
+#Moverse arriba
+bsim = tk.Button(master=form, text="▲", relief=tk.RAISED, command= arriba)
+bsim.place(x=80,y=70)
 
 #Generacion del mapa
 mapa = tk.Frame(master=window, width=40*20, height=40*10, borderwidth=1)
@@ -42,14 +115,17 @@ mapa.pack()
 
 for i in range(10):
     for j in range(20):
-        Loseta = tk.Button(master=mapa, width=4, height=2, text=f"{numlo}", bg="grey", fg="black", relief=tk.RAISED)
+        Loseta = tk.Button(master=mapa, width=4, height=2, text=f"{numlo} \n -", bg="grey", fg="black", relief=tk.RAISED)
         Loseta.bind("<Button-1>", left_click)
+        Loseta.bind("<Button-2>", mid_click)
         Loseta.bind("<Button-3>", right_click)
         Loseta.place(x=coordx,y=coordy)
+        Simbolos[numlo] = "-"
         Cuarto[numlo] = Loseta
         coordx+=40
         numlo+=1
     coordx=0
     coordy+=40
+Cuarto[0].config(text=botchita, fg='pink')
 
 window.mainloop()
